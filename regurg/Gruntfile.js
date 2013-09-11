d1d11871cea14d9f99ca5ca170688b681276d3e9
Chase Kernan <chase.kernan@gmail.com>
2013-09-11 18:27:38 -0400

    Regurg hello world working

---
+++ regurg/Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      scripts: {
+        files: ['js/**/*.js'],
+        tasks: ['jshint', 'browserify:regurg'],
+        options: { spawn: false, },
+      },
+    },
+    browserify: {
+      libs: {
+        options: {
+          shim: {
+            jquery: {
+              path: './libs/jquery.js',
+              exports: '$'
+            },
+            underscore: {
+              path: './libs/underscore.js',
+              exports: '_'
+            },
+            d3: {
+              path: './libs/d3.v3.js',
+              exports: '_'
+            },
+          }
+        },
+        src: ['./libs/*.js'],
+        dest: 'deploy/libs.js'
+      },
+      regurg: {
+        options: {
+          alias: [
+            './libs/jquery.js:jquery',
+            './libs/underscore.js:underscore',
+            './libs/d3.v3.js:d3'
+          ],
+          external: [
+            './libs/jquery.js',
+            './libs/underscore.js',
+            './libs/d3.v3.js'
+          ]
+        },
+        src: ['js/main.js'],
+        dest: 'deploy/main.js'
+      }
+    },
+    jshint: {
+      all: ['js/**/*.js'],
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserify:regurg']);
+
+};
